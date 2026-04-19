ceb54b6459f48fc211b14baf966fc08cdc8ca592
Pau Go Si <paugosi@hotmail.com>
2026-04-19 18:46:56 +0200

    Clean BookingsController

--- BookingSystem.Api/Controllers/BookingsController.cs
+++ BookingSystem.Api/Controllers/BookingsController.cs
@@ -3,24 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 namespace BookingSystem.Api.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class BookingsController : ControllerBase
     {
-        private static readonly string[] Summaries =
-        [
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        ];
-
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
-        }
     }
 }
