e813bdfea5c9036456fc7db2ddaef71506c0af52
Pau Go Si <paugosi@hotmail.com>
2026-08-25 19:59:08 +0200

    Allow optional resource description and location

--- BookingSystem.Api/Models/Resource.cs
+++ BookingSystem.Api/Models/Resource.cs
@@ -5,8 +5,8 @@
         public int Id { get; set; }
 
         public string Name { get; set; } = null!;
-        public string Description { get; set; } = null!;
-        public string Location { get; set; } = null!;
+        public string? Description { get; set; }
+        public string? Location { get; set; }
 
         public int Capacity { get; set; }
         public bool IsActive { get; set; }
